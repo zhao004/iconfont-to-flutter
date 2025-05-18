87d61705538a6abdef0c81980c8906c1c9409773
zhao <2909552840@qq.com>
2025-05-18 10:14:41 +0800

    add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/iconfont-to-flutter/',
   plugins: [react()],
   server: {
     port: 3000,
